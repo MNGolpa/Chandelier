5c867e7fada7c941fc2174d1b85beccd0bd0ba96
AAA <AAA@DESKTOP-TSMPA7L>
2022-04-17 17:51:39 +0430

    Add project files.

---
+++ ShopManagement.Application.Contracts/ProductCategory/IProductCategoryApplication.cs
@@ -0,0 +1,10 @@
+﻿namespace ShopManagement.Application.Contracts.ProductCategory
+{
+    public interface IProductCategoryApplication
+    {
+        void Add(AddProductCategory command);
+        void Edit(EditProductCategory command);
+        Domain.ProductCategoryAgg.ProductCategory GetDetails(long id);
+        List<ProductCategoryViewModel> Search(ProductCategorySearchModel searchModel);
+    }
+}
---
+++ ShopManagement.Domain/ProductCategoryAgg/IProductCategoryRepository.cs
@@ -0,0 +1,9 @@
+﻿namespace ShopManagement.Domain.ProductCategoryAgg
+{
+    public interface IProductCategoryRepository
+    {
+        void Add(ProductCategory entity);
+        ProductCategory GetDetails(long id);
+        List<ProductCategory> GetAll();
+    }
+}
